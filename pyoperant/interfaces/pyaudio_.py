ed705611ffe5c1bf2403f2739dee77f5ad732115
kevin <thekevinyu@gmail.com>
2019-02-25 12:41:01 -0800

    Pyaudio wavfile handling

--- pyoperant/interfaces/pyaudio_.py
+++ pyoperant/interfaces/pyaudio_.py
@@ -115,7 +115,7 @@ class PyAudioInterface(base_.AudioInterface):
         """
         def _callback(in_data, frame_count, time_info, status):
             if not self._playing_wav.is_set():
-                return (0, pyaudio.paComplete)
+                return (None, pyaudio.paComplete)
 
             try:
                 cont = self.callback()
@@ -126,7 +126,7 @@ class PyAudioInterface(base_.AudioInterface):
                 data = self.wf.readframes(frame_count)
                 return (data, pyaudio.paContinue)
             else:
-                return (0, pyaudio.paComplete)
+                return (None, pyaudio.paComplete)
 
         self.stream = self.pa.open(format=self.pa.get_format_from_width(self.wf.getsampwidth()),
                                    channels=self.wf.getnchannels(),
@@ -135,6 +135,7 @@ class PyAudioInterface(base_.AudioInterface):
                                    output_device_index=self.device_index,
                                    start=False,
                                    stream_callback=_callback)
+
         if start:
             self._play_wav(event=event)
 
@@ -155,6 +156,7 @@ class PyAudioInterface(base_.AudioInterface):
         try:
             logger.debug("Attempting to close pyaudio stream")
             events.write(event)
+            self.stream.stop_stream()
             self.stream.close()
             logger.debug("Stream closed")
         except AttributeError:
